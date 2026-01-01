f706ebebbcaa154ea3fbb11e6dfaa57736fd7107
Pradeep <pradeepspeedtest@gmail.com>
2026-01-01 07:58:12 +0530

    Fix leads array safety for production

--- pages/_app.js
+++ pages/_app.js
@@ -68,21 +68,28 @@ export default function App({ Component, pageProps }) {
         {/* DNS Prefetch - Performance */}
         <link rel="dns-prefetch" href="//www.googletagmanager.com" />
         <link rel="preconnect" href="https://www.googletagmanager.com" />
+
+        {/* ✅ Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NMK7MQ29');
+          `}
+        </Script>
       </Head>
 
-      {/* GA4 Script */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-Y66WTXV9XN"
-        strategy="afterInteractive"
-      />
-      <Script id="ga4" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-Y66WTXV9XN');
-        `}
-      </Script>
+         {/* ✅ Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NMK7MQ29"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
 
       {/* Main Content */}
       <main className={inter.variable}>

88d7d0da85d8a83647e9298e3273f6365d964d2b
Pradeep <pradeepspeedtest@gmail.com>
2026-01-01 08:42:46 +0530

    Fix

--- pages/_app.js
+++ pages/_app.js
@@ -69,17 +69,21 @@ export default function App({ Component, pageProps }) {
         <link rel="dns-prefetch" href="//www.googletagmanager.com" />
         <link rel="preconnect" href="https://www.googletagmanager.com" />
 
-        {/* ✅ Google Tag Manager */}
-        <Script id="gtm-head" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NMK7MQ29');
-          `}
-        </Script>
       </Head>
+            {/* Google Tag Manager - MAIN SCRIPT */}
+            <Script
+              id="gtm-init"
+              strategy="beforeInteractive"
+            >
+              {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NMK7MQ29');
+              `}
+            </Script>
+
 
          {/* ✅ Google Tag Manager (noscript) */}
       <noscript>
